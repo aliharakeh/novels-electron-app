158577ce947295bc45b8ebf55ba4368d28a93f1e
ali harakeh <34475779+aliharakeh@users.noreply.github.com>
2021-08-04 01:36:42 +0300

    init

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // mode: 'jit',
+  purge: ['./src/**/*.{html,ts}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {}
+  },
+  corePlugins: {
+    preflight: false
+  }
+};
