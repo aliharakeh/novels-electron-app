d0056b957de9c70b1929a175d0679694d7d170ff
ali harakeh <34475779+aliharakeh@users.noreply.github.com>
2021-08-04 03:06:05 +0300

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,23 @@ import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {SharedModule} from './shared/shared.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {AppRoutingModule} from './app-routing.module';
 
 import {AppComponent} from './app.component';
+import {PrimeNgModule} from './prime-ng/prime-ng.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
     SharedModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PrimeNgModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,10 @@
+import {NgModule} from '@angular/core';
+
+import {DropdownModule} from 'primeng/dropdown';
+
+@NgModule({
+  exports: [
+    DropdownModule
+  ]
+})
+export class PrimeNgModule {}
